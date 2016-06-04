b70eb7fa671f18beeefda45c6d8cda109d09fdaf
a1624 <a1624jz@aiit.ac.jp>
2016-06-03 20:15:29 -0700

    modify 03

--- src/main/java/jp/ac/aiit/nw/ex03/PhttpServer.java
+++ src/main/java/jp/ac/aiit/nw/ex03/PhttpServer.java
@@ -1,39 +1,47 @@
 package jp.ac.aiit.nw.ex03;
 
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.OutputStream;
-import java.net.InetSocketAddress;
-
-import com.sun.net.httpserver.HttpExchange;
-import com.sun.net.httpserver.HttpHandler;
-import com.sun.net.httpserver.HttpServer;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketException;
 
 /*
  * Simple http server
  */
 public class PhttpServer {
     public static final int PORT = 80; 
-
-	public static void main(String[] args) throws Exception {
+    public static void main (String args[]) {
     	int port = PORT;
     	try {
     		port = Integer.parseInt(args[0]);
     	} catch (Throwable e) {
     		System.err.println("Parameter error!");
     	}
-		HttpServer server = HttpServer.create(new InetSocketAddress(port), 0);
-		server.createContext("/", new MyHandler());
-		server.setExecutor(null);
-		server.start();
-	}
+        try {
+            // ソケットを生成
+            ServerSocket serverSocket = new ServerSocket(port);
+            // 入出力ストリームを用意し，クライアントからの要求を待つ
+            Socket socket = serverSocket.accept();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            PrintStream writer = new PrintStream(socket.getOutputStream());
 
-	static class MyHandler implements HttpHandler {
-		public void handle(HttpExchange t) throws IOException {
-			String response = "Hello!";
-			t.sendResponseHeaders(200, response.length());
-			OutputStream os = t.getResponseBody();
-			os.write(response.getBytes());
-			os.close();
-		}
-	}
+            System.out.println(reader.readLine());
+            System.out.println(reader.readLine());
+            writer.println("Hello!");
+
+            writer.close();
+            reader.close();
+            socket.close();
+            serverSocket.close();
+        } catch (SocketException e) {
+            System.err.println("Socket error");
+            System.exit(-1);
+        } catch (IOException e) {
+            System.err.println("IO error");
+            System.exit(-1);
+        }
+    }
 }
